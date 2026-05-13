8d092c6c9ff6532cbb7240d592107d37f91295d2
KAYAN0222 <aaaup19@gmail.com>
2026-05-13 22:22:58 +0300

    Update web.php

--- ebda3soft-backend/routes/web.php
+++ ebda3soft-backend/routes/web.php
@@ -1,19 +1,9 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Artisan;
 
-// الصفحة الرئيسية
 Route::get('/', function () {
-    return "مرحباً بك  ، مشروع softpro يعمل!";
+    return view('welcome'); // أو صفحة تسجيل الدخول الخاصة بمشروعك
 });
 
-// أمر الميغريشن الإجباري
-Route::get('/run-migration', function () {
-    try {
-        Artisan::call('migrate', ['--force' => true]);
-        return "تم إنشاء الجداول بنجاح!";
-    } catch (\Exception $e) {
-        // إظهار الخطأ الحقيقي مهما كان
-        return "فشل بسبب: " . $e->getMessage();
-    }
-});
+// هنا تبدأ بإضافة مسارات مشروع "إبداع سوفت" الفعلية
