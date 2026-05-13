e5105add6f990f9aaf2895927ddd392773f8108b
KAYAN0222 <aaaup19@gmail.com>
2026-05-13 21:44:48 +0300

    Fix route definition for migration endpoint

--- ebda3soft-backend/routes/web.php
+++ ebda3soft-backend/routes/web.php
@@ -4,7 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-    Route::get('/run-migration', function () {
+});
+
+Route::get('/run-migration', function () {
     \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
     return 'تم إنشاء جداول قاعدة البيانات بنجاح يا أحمد!';
 });
