ac52562ca37f7f0048cbf54965241cdf7bbec205
KAYAN0222 <aaaup19@gmail.com>
2026-05-13 21:43:12 +0300

    Add route for running database migrations

--- ebda3soft-backend/routes/web.php
+++ ebda3soft-backend/routes/web.php
@@ -4,4 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    Route::get('/run-migration', function () {
+    \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
+    return 'تم إنشاء جداول قاعدة البيانات بنجاح يا أحمد!';
 });
